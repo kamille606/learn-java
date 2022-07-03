680c5668d9fe5079a7b34035dfbc7f9297f9cb0d
Administrator <kamille606@163.com>
2022-07-04 02:08:19 +0800

    init

--- src/main/java/com/lain/learn/collection/LearnComparator.java
+++ src/main/java/com/lain/learn/collection/LearnComparator.java
@@ -1,5 +1,7 @@
 package com.lain.learn.collection;
 
+import com.lain.common.Student;
+
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -22,6 +24,16 @@ public class LearnComparator {
         list.sort(Comparator.comparing(String::length, Comparator.naturalOrder())
                 .thenComparing(String::length, Comparator.reverseOrder()));
         list.forEach(System.out::println);
+
+
+        List<Student> list2 = new ArrayList<>() {{
+            add(new Student("刘", 18, 90));
+            add(new Student("王", 20, 77));
+            add(new Student("李", 19, 77));
+        }};
+        list2.sort(Comparator.comparing(Student::getAge)
+                .thenComparing(Student::getScore));
+        list2.forEach(System.out::println);
     }
 
 }
---
+++ src/main/java/com/lain/learn/test/Test.java
@@ -0,0 +1,6 @@
+package com.lain.learn.test;
+
+public class Test {
+    public static void main(String[] args) {
+    }
+}
